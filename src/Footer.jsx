78d997f3b84a19a3360a1d4b5ea38f778aa4d8bf
David <david@tangentdigitalagency.com>
2021-10-19 13:33:07 -0700

    Updated Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -10,20 +10,17 @@ class Footer extends Component {
         return(
           
           <section className="py-20">
-          <div className="container mx-auto px-4">
+          <div className="container mx-auto px-4 pb-10">
             <div className="flex flex-wrap justify-between items-center -mx-4">
               <div className="w-full lg:w-auto px-4">
                 <a className="inline-block mb-6 text-gray-900 text-lg font-semibold" href="/" data-config-id="brand">
                   <img className="h-7" src={Logo} alt="" width="auto" />
                 </a>
                 <p className="hidden lg:block text-sm text-gray-500" data-config-id="copy">All rights reserved © Quotehound 2021</p>
-                <p className="hidden lg:block text-sm text-gray-500" data-config-id="copy">© Copyright 2021 Quotehound Inc., All rights reserved.
-Quotehound Inc. is not endorsed by or affiliated with Medicare or any government program.
-
-Invitations for application for insurance via Quotehound.com, usmedicarequotes.com, usahealthquotes.com, are made through Quotehound, Inc. or through its designated agent(s), Devanand Gaymes, only where licensed and appointed. License numbers are available here or upon request and are automatically provided where required by law.  Devanand Gaymes is a licensed General Lines Health Insurance agent in the State of Texas. Devanand Gaymes' Texas license number is 1897511.</p>
+                <p className="hidden lg:block text-sm text-gray-500" data-config-id="copy">Quotehound Inc. is not endorsed by or affiliated <br/> with Medicare or any government program.</p>
               </div>
 
-            
+              
               <div className="w-full lg:w-4/6 px-4">
                 
                 <div className="flex flex-wrap items-center justify-end">
@@ -35,11 +32,15 @@ Invitations for application for insurance via Quotehound.com, usmedicarequotes.c
                   <a className="inline-block mr-auto lg:mr-0 py-4 px-8 text-sm text-white font-medium leafing-normal bg-blue-500 hover:bg-blue-300 hover:shadow-lg rounded"  href="tel:8882922361" data-config-id="primary-action">Get Your Free Quote</a>
                 </div>
                 <p className="mt-6 lg:hidden text-sm text-gray-500" data-config-id="copy">All rights reserved © Quotehound 2021</p>
+                <p className="mt-6 lg:hidden text-sm text-gray-500" data-config-id="copy">Quotehound Inc. is not endorsed by or affiliated with Medicare or any government program.</p>
+
               </div>
             </div>
           </div>
           <div className="container mx-auto px-4 lg:w-1/2 md:w-full text-center">
           <p className="text-sm text-center text-gray-500"> This is a commercial site designed for the solicitation of insurance from selected health and Life insurance carriers. It is not an insurer, an insurance agency, or a medical provider. Insurance agency services may be provided by a partner licensed agency. This site is not maintained by or affiliated with the federal government’s Health Insurance Marketplace website or any state government health insurance marketplace</p>
+
+          <p className="text-sm text-center text-gray-500">Invitations for application for insurance via usmedicarequotes.comare made through Quotehound, Inc. or through its designated agent(s), Devanand Gaymes, only where licensed and appointed. License numbers are available here or upon request and are automatically provided where required by law.  Devanand Gaymes is a licensed General Lines Health Insurance agent in the State of Texas. Devanand Gaymes' Texas license number is 1897511</p>
           </div>
         </section>
     
