ac025810ef91274cfeb3e5a1cac723a45502fd72
tangentdigitalagency <david@tangentdigitalagency.com>
2021-10-29 13:26:56 -0700

    Button Change

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -89,7 +89,7 @@ class LandingPage extends Component {
 
 <div className="flex justify items-center formSection py-10">
 <input className="appearance-none w-1/2 p-3 text-lg font-semibold leading-none bg-white rounded zipInput " type="text" name="addressField" placeholder="Zip Code" pattern="\d*" value={this.state.value} id="zipCode" maxLength={5}/>
-<button className="px-6 py-4 mb-3 m-2 text-md font-bold bg-blue-400 hover:bg-blue-600 hover:shadow-lg text-white rounded transition duration-200 zipSubmit" type="submit">Start My Quote</button>
+<button className="px-6 py-4 mb-3 m-2 text-md font-bold bg-blue-400 hover:bg-blue-600 hover:shadow-lg text-white rounded transition duration-200 zipSubmit text-center" type="submit">Start Your No Obligation Free Quote</button>
 
 </div>
 
