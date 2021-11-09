f25ad50c60ca706f0ced19198cf33a79d1a65a17
tangentdigitalagency <david@tangentdigitalagency.com>
2021-11-08 20:01:48 -0700

    fixex

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -84,7 +84,7 @@ class LandingPage extends Component {
   <div className="relative z-10 container px-4 mx-auto">
     <div className="max-w-4xl pt-20">
       
-      <h2 className="mt-8 mb-8 text-5xl lg:text-7xl text-white font-bold" data-config-id="header">Compare Multiple Medicare Insurance Quotes</h2>
+      <h2 className="mt-8 mb-8 lg:text-7xl md:text-4xl sm:text-4xl xsm:text-4xl text-white font-bold" data-config-id="header">Compare Multiple Medicare Insurance Quotes</h2>
       <form onSubmit={this.nextStep} >
 
 <div className="flex justify items-center formSection py-10">
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,22 +8,29 @@ class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="relative py-4 px-4 lg:px-10 bg-white">
-    <div className="flex justify-between items-center">
+                <nav className="relative py-4 px-4 lg:px-10 sm:px-2 bg-white">
+    <div className="flex justify-between items-center display-grid">
       <a className="text-xl text-white font-semibold" href="#" data-config-id="brand">
-        <img className="h-12" src={Logo} alt="" />
+        <img className="h-12 md:h-8 sm:h-8" src={Logo} alt="" />
       </a>
        
+      {/* <a className="text-center inline-block px-2 py-2 text-sm text-white font-bold leading-loose bg-blue-400 hover:bg-blue-600 rounded transition duration-200" href="tel:8887013068" data-config-id="04_button">888-701-3068</a>  */}
+      
+      {/* <ul className="md:flex md:flex-row" id="mobileMenu">
 
-      <ul className="md:flex md:flex-row" id="mobileMenu">
-  <li className="pr-5"> 
-    <a className="text-center inline-block px-2 py-2 text-sm text-white font-bold leading-loose bg-blue-400 hover:bg-blue-600 rounded transition duration-200" href="tel:8887013068" data-config-id="04_button">888-701-3068 | TTY: 711</a> 
-  </li>  
-
+  <li className="inline-block px-2 py-2 text-sm text-gray-500 font-bold leading-loose"> TTY: 711 </li>
   <li className="inline-block px-2 py-2 text-sm text-gray-500 font-bold leading-loose"> 9AM - 6PM ET Monday - Friday </li>
 
-</ul>
+</ul> */}
 
+
+<div className="justify-self-end sm:grid flex flex-row items-center text-right">
+<a className="text-center inline-block px-2 py-2 text-sm text-white font-bold leading-loose bg-blue-400 hover:bg-blue-600 rounded transition duration-200" href="tel:8887013068" data-config-id="04_button">888-701-3068</a> 
+<p className="text-sm font-bold"> TTTY: 711</p>
+<p className="text-sm font-bold">9AM - 6PM ET <br/> Monday - Friday </p>
+
+
+</div>
 
 
     </div>
